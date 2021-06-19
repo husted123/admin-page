fea02ba7536e05e900d77be36d22ea3e0eeb7362
Noex98 <johannes.knickering@hotmail.com>
2021-06-19 19:17:39 +0200

    .

--- main.js
+++ main.js
@@ -1,66 +1,67 @@
-fetch('https://eksamen-api.herokuapp.com/api/contact')
+// Fetch contact info
+fetch('http://localhost:4000/api/contact')
   .then(response => response.json())
   .then(data => printContact(data))
-  
-  
 
-  let printContact = (data) => {
-    document.getElementById("number").placeholder = data.number
-    document.getElementById("email").placeholder = data.email
-  };
+// Display in placeholders
+let printContact = (data) => {
+  document.getElementById("number").placeholder = data.number
+  document.getElementById("email").placeholder = data.email
+};
 
-
-fetch('https://eksamen-api.herokuapp.com/api/img')
+// Fetch image list
+fetch('http://localhost:4000/api/img')
  .then(response => response.json())
  .then(data => printImg(data));
 
+// Display in dom
 function printImg(data){
   data.map(img => {
-    console.log(img)
+    // Container for image card
     let container = document.createElement("div")
+    document.getElementById("imgContainer").appendChild(container)
 
+    //Image
     let image = document.createElement("img")
-    image.setAttribute('src', 'https://eksamen-api.herokuapp.com' + img.url);
+    image.setAttribute('src', 'http://localhost:4000' + img.url);
     image.setAttribute('alt', img.alt);
     image.setAttribute('tag', img.tag);
-
-    let urlDisplay = document.createElement("div");
-    urlDisplay.innerText = 'Url: ' + img.url;
-
-    let altDisplay = document.createElement("div");
-    altDisplay.innerText = 'alt: ' + img.alt;
-
-    let tagDisplay = document.createElement("div");
-    tagDisplay.innerText = 'tag: ' + img.tag;
-    
-    let deleteBtn = document.createElement("button");
-    deleteBtn.innerText = 'Slet billede'
-    /*deleteBtn.onclick = () => deleteImg()
-
-    let deleteData = {
-      url: img.url,
-    }
-
-    function deleteImg(){
-      fetch('https://eksamen-api.herokuapp.com/api/img', {
-        method: "delete",
-        body: JSON.stringify(deleteData)
-      })
-      .catch(err => console.log(err))
-    } */
-
-    document.getElementById("imgContainer").appendChild(container)
     container.appendChild(image)
 
+    // Display URL
+    let urlDisplay = document.createElement("div");
+    urlDisplay.innerText = 'Url: ' + img.url;
     container.appendChild(urlDisplay)
+
+    //Display Alt text
+    let altDisplay = document.createElement("div");
+    altDisplay.innerText = 'alt: ' + img.alt;
     container.appendChild(altDisplay)
+
+    // Display tag
+    let tagDisplay = document.createElement("div");
+    tagDisplay.innerText = 'tag: ' + img.tag;
     container.appendChild(tagDisplay)
+
+    // Delte btn
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = 'Slet billede'
+    deleteBtn.onclick = () => {
+        deleteImg(img.url, ).then(data => console.log(data))
+        container.style.display = 'none';
+    }
     container.appendChild(deleteBtn)
     
   })
 }
 
-
-
-  
-  
+async function deleteImg(data){
+    const response = await(fetch('http://localhost:4000/api/img', {
+        method: 'delete',
+        body: JSON.stringify({'url': data}),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }))
+    console.log(response.json())
+}
